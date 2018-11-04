d6e885dda2738f0a3aaa80fe38002454c33342fc
Joseph CHOTARD <josephchotard@Josephs-MacBook-Pro.local>
2018-11-04 11:51:18 +0000

    Fixed arument error

--- util.py
+++ util.py
@@ -16,7 +16,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
 def file_exists(*args):
     if len(args) == 2 and isinstance(args[0], str) and isinstance(args[1], str):
-        filepath = "gs://{}/{}".format(bucket_name, source_file_name)
+        filepath = "gs://{}/{}".format(args[0], args[1])
     if len(args) == 1 and isinstance(args[0], str):
         try:
             gcs.stat(filepath)
