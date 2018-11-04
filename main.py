ede1423ba288d4515c538e0dd8a4f0d751005ad7
Joseph CHOTARD <josephchotard@Josephs-MacBook-Pro.local>
2018-11-04 10:06:13 +0000

    Added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from upload import upload_blob
+from parser import *
+
+def update_videos(username, password):
+    get_wav(get_videos(username, password))
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.wav'):
+                upload_blob("audioforhtm", file, file);
+
+if __name__=='__main__':
+    username = raw_input("Username: ")
+    password = getpass.getpass("Password: ")
+    update_videos(username, password)
